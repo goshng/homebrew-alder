495c6417f80d4fcefb3bde896691a67163843465
Sang Chul Choi <goshng@yahoo.co.kr>
2013-09-07 01:18:35 -0800

    Alder dist. name change.

--- alder.rb
+++ alder.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Alder < Formula
   homepage 'https://bitbucket.org/goshng/alder/wiki/Home'
-  url 'https://bitbucket.org/goshng/alder/downloads/alder-pre-0.3.2-Source.tar.gz'
-  sha1 '6c534908dc136962f5a87121a81be6cdcf6621f9'
+  url 'https://bitbucket.org/goshng/alder/downloads/alder_pre-0.3.2.tar.gz'
+  sha1 '988ee9303627f9c23e503975c2f55ead0b6c3363'
 
   depends_on 'cmake' => :build
 
