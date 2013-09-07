a6947f934396325213241000a139538d22aa25d5
Sang Chul Choi <goshng@yahoo.co.kr>
2013-09-07 02:00:51 -0800

    Alder 0.3.2 - bug in alder_fastq_pair.

--- alder.rb
+++ alder.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Alder < Formula
   homepage 'https://bitbucket.org/goshng/alder/wiki/Home'
   url 'https://bitbucket.org/goshng/alder/downloads/alder_pre-0.3.2.tar.gz'
-  sha1 '988ee9303627f9c23e503975c2f55ead0b6c3363'
+  sha1 '80dd6b5fe4daac7adf17acf1596c854e0e30955e'
 
   depends_on 'cmake' => :build
 
