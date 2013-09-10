5ec3b3387acaf580aee9c3a1117d91f53c30b105
Sang Chul Choi <goshng@yahoo.co.kr>
2013-09-10 09:49:02 -0800

    Cutadapt 1.2.1 - trims adapter sequences in high-throughput sequencing data.

--- cutadapt.rb
+++ cutadapt.rb
@@ -5,7 +5,7 @@ class Cutadapt < Formula
   url 'http://cutadapt.googlecode.com/files/cutadapt-1.2.1.tar.gz'
   sha1 '35d912ab524238a145c05bc5dd460b1177a3cf97'
 
-  depends_on python
+  depends_on 'python'
 
   def install
     system "python", "setup.py", "build"
